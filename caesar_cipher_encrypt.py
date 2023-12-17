d5df166528690cc2f76a285dde2b9b8ad84c45ec
EyeCode4You_Host <77638909+eyecode4you@users.noreply.github.com>
2023-12-17 10:13:54 +0000

    Update caesar_cipher_encrypt.py

--- caesar_cipher_encrypt.py
+++ caesar_cipher_encrypt.py
@@ -1,29 +1,29 @@
-#caesar_cipher_encrypt.py
+#ainteractive_caesar_cipher_encrypt.py
 #eyecode4you 17/12/23
-CHARSET = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
-CIPHER = 0
-MESSAGE = ""
+CHARSET = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")	
+KEY = 0						#Character shift value
+MESSAGE = ""					#Message to encrypt
 
 def main(args):
     return 0
 
 if __name__ == '__main__':
-	while not CIPHER in range(1,27):
+	while not KEY in range(1,27):		#Valid range is 1-26 (27 would cause index out of bound)
 		try:
-			CIPHER = int(input("ENTER CIPHER VALUE (1-26): "))
+			KEY = int(input("ENTER KEY VALUE (1-26): "))
 		except:
 			print()
 	MESSAGE = input("ENTER MESSAGE TO ENCRYPT: ")
 	
-	print("\n****MAPPING****")
+	print("\n****MAPPING****")		#Show new character mapping
 	print(CHARSET)
 	for i in CHARSET:
-		print(CHARSET[CHARSET.index(i) - CIPHER], end='')
-	print("\n****MAPPING****\n\n")
+		print(CHARSET[CHARSET.index(i) - KEY], end='')
+	print("\n")
 	
-	print("ENCRYPTED MESSAGE: ", end='')
-	for i in MESSAGE.upper():
+	print("ENCRYPTED MESSAGE (CIPHER): ", end='')
+	for i in MESSAGE.upper():		#Replace spaces with '-'
 		if i not in CHARSET:
 			print('-', end='')
 			continue
-		print(CHARSET[CHARSET.index(i) - CIPHER], end='')
+		print(CHARSET[CHARSET.index(i) - KEY], end='')
